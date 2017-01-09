ad9352774b5ea57926dbd91bed1ada4e9611e4d0
Poonam Patil <poonampatil0710@gmail.com>
2017-01-09 18:04:45 +0530

    1.Added program for PANcard number validation.

---
+++ panvalidate.py
@@ -0,0 +1,18 @@
+"""
+This is code for PAN card validation.
+"""
+
+import datetime
+__author__= 'Poonam'
+
+def panvalidate():
+	Pan1= raw_input("Please Enter PAN Number: ")
+	Pan= Pan1.upper()
+	
+	fourth = Pan[3].endswith('P') or Pan[3].endswith('F') or Pan[3].endswith('C') 
+	if(len(Pan)==10 and Pan[0:5].isalpha() and fourth and Pan[5:9].isdigit() and Pan[9].isalpha()):	
+		print 'valid'		
+	else:
+		print 'notvalid'
+if __name__== "__main__":
+	panvalidate()
