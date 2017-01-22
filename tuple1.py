29d62857277f425087cd5954646dcdff697755d8
Poonam Patil <poonampatil0710@gmail.com>
2017-01-22 10:15:41 +0530

    1.Added the program for demonstration of use of tuple.

---
+++ tuple1.py
@@ -0,0 +1,15 @@
+"""
+This is a program for demonstration of use of tuple.
+"""
+
+__author__= 'Poonam'
+
+
+tuple = (3, 'Poonam', 3.24, "hi hello")
+tuple1 = (23, 'Patil')   
+print(tuple)
+print(tuple[0])
+print(tuple[1:4])
+print(tuple [2:])
+print(tuple1 * 2)
+print(tuple + tuple1)
